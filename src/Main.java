a57ae3dd9577c9357d8ac10a18e98a7df8195bf4
zhongmouhuang <zhongmouh2027@student.cis.edu.hk>
2024-08-28 09:38:45 +0800

    hi

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Zhongmou H
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+            String firstName = new String();
+            String favoriteFood = new String();
+
+
+            System.out.println("What is your first name?");
+            Scanner scanner = new Scanner(System.in);
+            firstName = scanner.nextLine();
 
 
 
 
+            System.out.println("What is your age?");
+
+            int age = scanner.nextInt();
+
+            System.out.println("What is your fav food?");
+            Scanner sc = new Scanner(System.in);
+            favoriteFood = sc.nextLine();
+
+            String details = "Name: " + firstName + "\nAge: " + age + "\nFavorite food: " + favoriteFood;
+
+            System.out.println(details);
     }
 
 }
